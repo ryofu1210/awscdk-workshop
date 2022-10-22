af64ff503da7a31fa96ec82eedfc19294e4dd37b
Ryosuke Fujii <ryosuke.fujii@mazrica.com>
2022-10-23 01:39:28 +0900

    deploy lambda

--- lib/awscdk-workshop-stack.ts
+++ lib/awscdk-workshop-stack.ts
@@ -1,8 +1,15 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AwscdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
   }
 }
